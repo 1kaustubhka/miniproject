e9b1cea0ff81e44b9ef8ac2af4bf7f2eba37a961
Kaustubh Shreepad Kakade <kaustubhka@cybage.com>
2021-01-13 20:31:48 +0530

    Merge code with Anushka

--- Kaustubh/src/js/index.js
+++ Kaustubh/src/js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
-    var th = 0;//theme flag
 
+    var th = 0;
+    localStorage.setItem("theme", th);
     $("nav").load('nav_bar.html', () => {//navbar jquery comes here
      //PROFILE BUTTON  
         $("#profile").click(() => {
@@ -16,9 +17,43 @@ $(document).ready(() => {
 
     //HOME BUTTON
         $("#home").click(() => {
-            
-            document.location.href="/html/index.html";
+            $("section").load('categories.html');
             //I CAN REFRESH.
+            $.ajax({
+                url: "http://localhost:3000/Category/",
+                type: "GET",
+                dataType: "json",
+                contentType: "application/json",
+                success: (cat) => {
+        
+                    for (let i = 0; i < cat.length; i++) {
+                        var ct = cat[i]['id'].toString();
+                        console.log(ct);
+                        var cid = ct + "_testsets";
+                        //alert(cid);
+                        $('section').append(`
+                    <div class="col-lg-6 col-sm-12 gy-1">
+                                <div class="card">
+                                    <img src="../images/c++.png" class="card-img-top" alt="===">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${ct}</h5>
+                                        <p class="card-text">Test Your Knowledge On ${ct}</p>
+                                        <button  id=${cid} class="btn btn-light crdbtn">Take Test</button>
+                                    </div>
+                                </div>
+                            </div>`);
+        
+                    }
+        
+                
+                },
+        
+            });
+            $(document).on("click", (".crdbtn"), (function(){
+                alert(this.id);
+                $("section").load('categoriestestset.html');
+            })
+            );
         })
     });
 
@@ -73,6 +108,46 @@ $(document).ready(() => {
         // $("#html_testsets").click(()=>{
         //     $("section").load('categoriestestset.html');
         // })
+        $("#theme").click(() => {
+            if (th == 0) {
+                th = 1;
+                localStorage.setItem("theme", th);
+                $("header").css("background-color", "#282828");
+                $("nav").css("background-color", "#282828");
+                $("section").css("background-color", "#282828");
+                $("header").css("color", "white");
+                $(".card").css({
+                    "background-color": "#282828",
+                    "color": "white",
+                    "border": "2px black gray"
+                });
+                $("button").css({
+                    "background-color": "#292b2c",
+                    "color": "#f7f7f7"
+                });
+                $(".form-label").css("color", "white");
+    
+    
+            } else {
+                th = 0;
+                localStorage.setItem("theme", th);
+                $("header").css("background-color", "white");
+                $("nav").css("background-color", "white");
+                $("section").css("background-color", "white");
+                $("header").css("color", "black");
+                $(".card").css({
+                    "background-color": "white",
+                    "color": "black",
+                    "border": "2px black gray"
+                });
+                // $(".crdbtn").addClass("btn btn-light");
+                $(".form-label").css("color", "black");
+                $("button").css({
+                    "background-color": "#f7f7f7",
+                    "color": "#292b2c"
+                });
+            }
+        })
 });
 
 
