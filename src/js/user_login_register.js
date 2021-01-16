d1719efdfe570696254ad14d47e1487aae0b5fd7
Shivam Laxminarayan Sahu <shivamsah@cybage.com>
2021-01-16 21:24:02 +0530

    user login modified

--- src/js/user_login_register.js
+++ src/js/user_login_register.js
@@ -1,112 +1,137 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyAuKxm63mxhiDIW27ITZVYicQaLd5tkX-M",
-  authDomain: "newsign-6fc38.firebaseapp.com",
-  databaseURL: "https://newsign-6fc38-default-rtdb.firebaseio.com",
-  projectId: "newsign-6fc38",
-  storageBucket: "newsign-6fc38.appspot.com",
-  messagingSenderId: "684095806892",
-  appId: "1:684095806892:web:a8e8e62deab08461122e09",
-  measurementId: "G-7LC2FFM16B"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+$(document).ready(() => {
 
+  var arr = new Array();
 
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    var database = firebase.database();
-    function writeUserData(email) {
-      firebase.database().ref('Email' + email).set({
-        email: email,
-      });
-    }
-  } else {
-    // No user is signed in.
-  }
-});
+  $.ajax({
+      type: "GET",
+      url: "http://localhost:3000/users",
 
-function login() {
+      success: function (data, status, xhr) {
 
-  var userEmail = document.getElementById("email").value;
-  var userPassword = document.getElementById("password").value;
+          arr = JSON.parse(data);
+          console.log(arr);
 
+      },
 
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPassword)
-    .then((user) => {
-      window.location.replace("logout.html");
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-
-      window.alert("Please Enter Correct Login Details")
-
-    });
-}
-
-function logout() {
-
-  firebase.auth().signOut().then(() => {
-    window.location.href("signin.html");
-
-  }).catch((error) => {
+      error: function (jqXhr, textStatus, errorMessage) {
+          console.log('error' + errorMessage);
+      },
+      dataType: "text",
+      contentType: "application/json",
   });
 
-}
+  $('footer').load('footer.html ', function () {
 
-function register() {
-  var uname = document.getElementById("name").value;
-  var uemail = document.getElementById("email").value;
-  var ucontact = document.getElementById("contact").value;
-  var upassword = document.getElementById("password").value;
-
-  firebase.auth().createUserWithEmailAndPassword(uemail, upassword)
-    .then(function () {
-      window.alert("You Are Sucessfully Registered, Please Login")
-
-      var id = firebase.auth().currentUser.uid;
-      firebase.database().ref('Quiz_Users/' + id).set({
-        name: uname,
-        email: uemail,
-        contact: ucontact,
-      });
-    })
-    .catch(function (error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ..
-    });
-
-}
-
-
-function forgetpassword() {
-  var email = document.getElementById('email').value;
-  firebase.auth().sendPasswordResetEmail(email).then(function () {
-    alert('Password Reset Email Sent!');
-  }).catch(function (error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    if (errorCode == 'auth/invalid-email') {
-      alert(errorMessage);
-    } else if (errorCode == 'auth/user-not-found') {
-      alert(errorMessage);
-    }
-    console.log(error);
+  })
+  //password checking
+  $('#pass,#cpass').on('keyup', function () {
+      if ($('#pass').val() === $('#cpass').val()) {
+          $('#out').html("Matching");
+          $('#out').css('color', 'green');
+          $('#but').removeAttr("disabled");
+      }
+      else {
+          $('#out').html("Not Matching");
+          $('#out').css('color', 'red');
+          $('#but').attr("disabled", "true");
+      }
   });
-}
 
-function update() {
-Ready();
-var uname = document.getElementById("name").value;
-var uemail = document.getElementById("email").value;
-var ucontact = document.getElementById("contact").value;
-var id = firebase.auth().currentUser.uid;
 
-firebase.database().red('Quiz_Users/' +id).update({
-  name: uname,
-  email: uemail,
-  contact: ucontact,
-});
-}
 
+  //registration form submission
+  $('#register').submit((a) => {
+      a.preventDefault();
+      let name = $('#name').val();
+      let email = $('#email').val();
+      let password = $('#pass').val();
+
+      let phone = $('#phone').val();
+      let gender = $('input[name="gender"]:checked').val();
+
+      var user = {
+          "name": name, 
+          "email": email, 
+          "password": password, 
+          "phone": phone,
+      };
+
+      $.ajax({
+          type: "POST",
+          url: "http://localhost:3000/users",
+          data: JSON.stringify(user),
+          success: function (data, status, xhr) {
+          },
+
+          error: function (jqXhr, textStatus, errorMessage) {
+              console.log('error' + errorMessage);
+          },
+          dataType: "text",
+          contentType: "application/json",
+      });
+  })
+
+  //login form submission
+  $('#login').submit((a) => {
+      a.preventDefault();
+      let email = $('#email1').val();
+      let password = $('#pass1').val();
+      if (email == "" && password == "" || email == "" || password == "") {
+          alert("name and password is incorrect")
+      }
+      else {
+          $.ajax({
+              type: "GET",
+              url: "http://localhost:3000/users",
+              data: { "email": email, "password": password },
+              success: function (data, status, xhr) {
+                  console.log(data);
+                  if (data !== '[]') {
+                      window.location.replace('user_dashboard.html');
+                  }
+                  else {
+                      $('.text-muted').html('Wrong Email Id or Password');
+                      $('.text-muted').css('color', 'red');
+                      alert('error');
+                  }
+              },
+
+              error: function (jqXhr, textStatus, errorMessage) {
+                  console.log('error' + errorMessage);
+              },
+              dataType: "text",
+              contentType: "application/json",
+
+          });
+      }
+  })
+  
+  $('#email').on('keyup', function () {
+
+      var email = $('#email').val();
+      console.log(email);
+      var flag = 0;
+      for (var i = 0; i < arr.length; i++) {
+          if (email == arr[i].email) {
+              $('#inval').html("Already registered user");
+              $('#inval').css('color', 'red');
+              $('#but').attr("disabled", "true");
+              flag = 1;
+              break;
+          }
+
+      }
+      if (flag == 0) {
+          $('#but').removeAttr("disabled");
+          $('#inval').html("Ready to go");
+          $('#inval').css('color', 'green');
+
+      }
+      if (email == "") {
+          $('#but').attr("disabled", "true");
+          $('#inval').html("enter your email");
+          $('#inval').css('color', 'red');
+      }
+  })
+
+})
